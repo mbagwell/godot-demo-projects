3d69a0b99320173832f5355329fd31bb6b820881
mbagwell <43613846+mbagwell@users.noreply.github.com>
2020-04-17 13:31:46 -0400

    Reduces rate the clouds dims/shines(5s) and gives delay in between(20s)

--- 2d/platformer/src/Level/Clouds.gd
+++ 2d/platformer/src/Level/Clouds.gd
@@ -12,13 +12,14 @@ func _ready():
 	var N = 1;
 	while(INTEGER !=0):
 		modulate = Color(1,1,1,1)
-		
+		yield(get_tree().create_timer(20.0), "timeout")
 		for n in range(1,20):
-			yield(get_tree().create_timer(1.0), "timeout")
+			yield(get_tree().create_timer(5.0), "timeout")
 			N = N * 0.6
 			modulate = Color(1,1,1,N)
+		yield(get_tree().create_timer(20.0), "timeout")
 		for n in range(1,20):
-			yield(get_tree().create_timer(1.0), "timeout")
+			yield(get_tree().create_timer(5.0), "timeout")
 			N = N / 0.6
 			modulate = Color(1,1,1,N)
 		 # Replace with function body.
