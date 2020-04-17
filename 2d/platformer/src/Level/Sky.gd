266e44d20b87f277538e6710bfe6bdd1307d0be6
mbagwell <43613846+mbagwell@users.noreply.github.com>
2020-04-17 13:33:36 -0400

    Reduces rate the sky dims/shines(5s) and gives delay in between(20s)

--- 2d/platformer/src/Level/Sky.gd
+++ 2d/platformer/src/Level/Sky.gd
@@ -12,13 +12,14 @@ func _ready():
 	var N = 1;
 	while(INTEGER !=0):
 		modulate = Color(N,N,N)
-		
+		yield(get_tree().create_timer(20.0), "timeout")
 		for n in range(1,20):
-			yield(get_tree().create_timer(1.0), "timeout")
+			yield(get_tree().create_timer(5.0), "timeout")
 			N = N * 0.9
 			modulate = Color(N,N,N)
+		yield(get_tree().create_timer(20.0), "timeout")
 		for n in range(1,20):
-			yield(get_tree().create_timer(1.0), "timeout")
+			yield(get_tree().create_timer(5.0), "timeout")
 			N = N / 0.9
 			modulate = Color(N,N,N)
 		 # Replace with function body.
